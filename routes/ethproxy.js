5ca7447e4dc243dafa93363db61dc48fd9040525
olekon <kondrahanov@gmail.com>
2019-11-01 14:09:00 +0700

    infura api key fixed

--- routes/ethproxy.js
+++ routes/ethproxy.js
@@ -6,11 +6,16 @@ const express = require('express');
 const proxy = require('express-http-proxy');
 const config = require('../config');
 const router = express.Router();
+const apiKeys = require('../apikeys.js');
 
-router.use('/mainnet', proxy(config.nodes.mainnet));
-router.use('/ropsten', proxy(config.nodes.ropsten));
-router.use('/rinkeby', proxy(config.nodes.rinkeby));
-router.use('/kovan', proxy(config.nodes.kovan));
-router.use('/goerli', proxy(config.nodes.goerli));
+const proxyReqPathResolver = req => {
+    return `/v3/${apiKeys.infura}`;
+}
+
+router.use('/mainnet', proxy(config.nodes.mainnet, {proxyReqPathResolver}));
+router.use('/ropsten', proxy(config.nodes.ropsten, {proxyReqPathResolver}));
+router.use('/rinkeby', proxy(config.nodes.rinkeby, {proxyReqPathResolver}));
+router.use('/kovan', proxy(config.nodes.kovan, {proxyReqPathResolver}));
+router.use('/goerli', proxy(config.nodes.goerli, {proxyReqPathResolver}));
 
 module.exports = router;
